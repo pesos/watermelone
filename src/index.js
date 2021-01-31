b9fbb6fb7d2e6bfb51972175e7f62c2c9b3789ce
lucasace <tauro.royston@gmail.com>
2021-01-31 18:48:59 +0530

    [FEATURE] Added verification of account

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 var get_info = require("./pull.js");
+var get_user = require("./verify.js");
 
 (async () => {
-  console.log(await get_info("lucasace))
+  const user = await get_user("lucasace");
+  console.log(user.status);
+  // 200-> user exists
+  // 404-> user doesnt exist
+  console.log(await get_info("lucasace"));
 })()
+
