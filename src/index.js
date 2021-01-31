350bb5ce98266b12be4c4eecf7f223db65a77b86
Kunal Bhat <kunal.bhat2001@gmail.com>
2021-01-31 10:21:25 +0530

    Initial slash command support

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { slackEvents } = require('./events');
+const commandsRouter = require("./commands");
 
 const PORT = process.env.PORT || 8080;
 
@@ -7,19 +8,12 @@ const app = express();
 
 app.use("/api/events/", slackEvents.requestListener());
 app.use(express.json());
+app.use("/api/commands/", commandsRouter);
 
 app.get('/', (req, res) => {
-  res.status(200).send("App statu: Online");
+  res.status(200).send("App status: Online");
 })
 
 app.listen(PORT, async () => {
-  /*
-  await webClient.chat
-    .postMessage({
-      channel: "#general",
-      text: "PES Open Source bot online!",
-    })
-    .catch(console.error);
-    */
   console.log(`PES Open Source Slack Bot running on ${PORT}`);
 });
