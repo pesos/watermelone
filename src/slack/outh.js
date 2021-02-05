03d326b8770cba95747d1cf6661b856d992dfb24
Kunal Bhat <kunal.bhat2001@gmail.com>
2021-02-05 23:25:12 +0530

    Error handling for auth redirect

--- src/slack/outh.js
+++ src/slack/outh.js
@@ -3,6 +3,10 @@ const { callOauth } = require("./webclient");
 const router = express.Router();
 
 router.get('/', async (req,res) => {
+    if(req.params.error) {
+        res.status(401).send("Cannot authorise.");
+        return;
+    }
     var r = await callOauth(req.params.code);
     if(r) res.status(200).send("Auth completed.");
 })
